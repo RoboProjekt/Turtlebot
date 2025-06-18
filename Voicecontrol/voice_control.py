58ef630cb741547d4934fb9852945152de5eaadf
RoboProjekt <baau1001@stud.hs-kl.de>
2025-06-18 14:37:00 +0200

    Update

--- Voicecontrol/voice_control.py
+++ Voicecontrol/voice_control.py
@@ -57,7 +57,7 @@ class VoiceControlNode(Node):
         elif User == "bastian":
             model_path = r"/home/basti/Schreibtisch/Turtlebot/Voicecontrol/vosk-model-de-0.15"
         elif User == "pi":
-            model_path =r"/home/pi/Git_Turtlebot/Turtlebot/Voicecontrol/vosk-model-de-0.15"
+            model_path =r"/home/pi/Git_Turtlebot/Turtlebot/Voicecontrol/vosk-model-small-de-0.15"
 
         self.model = vosk.Model(model_path)
         self.navigator = BasicNavigator()
