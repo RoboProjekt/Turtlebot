d1726ecd540b3d3f72e99febf2e76a155952389f
RoboProjekt <baau1001@stud.hs-kl.de>
2025-07-29 13:38:42 +0200

    Hinzufügen der map_saves und der neuen Karte

---
+++ map_saves/Pixel_in_Weltkoordinaten.py
@@ -0,0 +1,20 @@
+import imageio
+
+# Konfiguration aus YAML
+origin = [-10.1, -4.3]  # (x, y)
+resolution = 0.05  # m/pixel
+image_path = "/home/andy/turtlebot3_ws/map_saves/H0.093.pgm"
+
+x_Pixel = int(input("Bitte x_Pixel eingeben eingeben: "))
+y_Pixel = int(input("Bitte y_Pixel eingeben: "))
+
+# Bildgröße ermitteln
+img = imageio.imread(image_path)
+height = img.shape[0]
+
+def pixel_to_world(px, py):
+    wx = origin[0] + (px * resolution)
+    wy = origin[1] + ((height - py) * resolution)
+    return round(wx, 3), round(wy, 3)
+
+print(pixel_to_world(x_Pixel,y_Pixel))
