64e12631eab44ee04df345118a61c8a1d11818c1
RoboProjekt <baau1001@stud.hs-kl.de>
2025-08-04 12:03:21 +0200

    Yolo Integration in voice_control_basti_040825 und Anbindung an main

--- Python_codes/Turtlebot_main.py
+++ Python_codes/Turtlebot_main.py
@@ -2,6 +2,7 @@ import rclpy                                        # type: ignore
 from rclpy.executors import MultiThreadedExecutor   #type:ignore
 import sys
 
+from voice_control_basti_040825 import VoiceControlNodeBasti #New
 from voice_control import VoiceControlNode   # type: ignore
 from battery_warning import BatteryMonitor   # type: ignore
 # from darknet_publisher.darknet_pub import YoloPublisher
@@ -11,13 +12,15 @@ def main(args=None):
 
     # Nodes initialisieren
     voice_node = VoiceControlNode()
+    voice_node_basti = VoiceControlNodeBasti()
     battery_warning = BatteryMonitor()
     # object_detect_node = YoloPublisher()
 
     # Multi-Threaded-Executor für parallele Verarbeitung
     executor = MultiThreadedExecutor()
     executor.add_node(battery_warning)
-    executor.add_node(voice_node)
+    #executor.add_node(voice_node)
+    executor.add_node(voice_node_basti)w
     # executor.add_node(object_detect_node)
 
     try:
@@ -27,6 +30,7 @@ def main(args=None):
     finally:
         executor.shutdown()
         voice_node.destroy_node()
+        voice_node_basti.destroy_node()
         battery_warning.destroy_node()
         # object_detect_node.destroy_node()
         rclpy.shutdown()
