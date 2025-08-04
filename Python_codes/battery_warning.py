cab7b36c3e53df93d0bccd403f80a5db09fe7c3d
RoboProjekt <baau1001@stud.hs-kl.de>
2025-08-04 10:07:57 +0200

    Battery Warning Update

--- Python_codes/battery_warning.py
+++ Python_codes/battery_warning.py
@@ -5,19 +5,21 @@ from sensor_msgs.msg import BatteryState    # type:ignore
 from turtlebot3_msgs.srv import Sound       # type:ignore
 import time
 
-
 class BatteryMonitor(Node):
     def __init__(self):
         super().__init__('battery_monitor')
 
-        # Parameter
+        # Schwelle für Warnung
         self.warning_threshold = 0.5  # 50 %
         self.warning_interval = 120   # alle 120 s
         self.last_warning_time = 0.0
 
+        # Letzter bekannter Akkustand
+        self.last_percentage = None
+
         # Sound-Client
         self.sound_client = self.create_client(Sound, '/sound')
-        
+
         # Subscriber auf Akkustand
         self.battery_sub = self.create_subscription(
             BatteryState,
@@ -26,20 +28,30 @@ class BatteryMonitor(Node):
             10
         )
 
+        # Timer für regelmäßige Statusausgabe (alle 300 Sekunden = 5 Minuten)
+        self.status_timer = self.create_timer(
+            300.0,
+            self.status_output_callback
+        )
+
     def play_sound(self, sound_value=2):
         request = Sound.Request()
         request.value = sound_value
-        future = self.sound_client.call_async(request)
-
+        self.sound_client.call_async(request)
 
     def battery_callback(self, msg: BatteryState):
         percentage = msg.percentage
+        self.last_percentage = percentage
         now = time.time()
-
-        self.get_logger().info(f'Akkuladung: {percentage * 100:.1f}%')
 
         if percentage < self.warning_threshold and now - self.last_warning_time > self.warning_interval:
             self.get_logger().warn('🔋 Akku fast leer! Warnung wird ausgegeben.')
             self.play_sound(3)
             self.last_warning_time = now
+
+    def status_output_callback(self):
+        if self.last_percentage is not None:
+            self.get_logger().info(f'⏱ Regelmäßige Ausgabe: Akkuladung bei {self.last_percentage * 100:.1f}%')
+        else:
+            self.get_logger().info('⏱ Noch kein Akkustand verfügbar.')
 
