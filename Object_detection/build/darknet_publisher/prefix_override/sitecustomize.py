b1b80dcdfa89cdffb8515ce17f1d46b0e317664e
RoboProjekt <baau1001@stud.hs-kl.de>
2025-08-06 15:20:17 +0200

    Angepasste Abhängigkeiten

--- Object_detection/build/darknet_publisher/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/basti/Schreibtisch/Turtlebot/Object_detection/install/darknet_publisher'
--- Object_detection/build/darknet_publisher/share/darknet_publisher/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/basti/Schreibtisch/Turtlebot/Object_detection/build/darknet_publisher"
