9d97ee23b12975478771c65842ecee8e95616836
RoboProjekt <baau1001@stud.hs-kl.de>
2025-08-12 11:05:27 +0200

    Update TurtlebotMain

--- Python_codes/Turtlebot_main.py
+++ Python_codes/Turtlebot_main.py
@@ -2,7 +2,7 @@ import rclpy                                        # type: ignore
 from rclpy.executors import MultiThreadedExecutor   #type:ignore
 import sys
 
-from voice_control_basti_040825 import VoiceControlNodeBasti #type:ignore
+from voice_control import VoiceControlNode #type:ignore
 from battery_warning import BatteryMonitor   # type: ignore
 
 
@@ -10,7 +10,7 @@ def main(args=None):
     rclpy.init(args=args)
 
     # Nodes initialisieren
-    voice_node = VoiceControlNodeBasti()
+    voice_node = VoiceControlNode()
     battery_warning = BatteryMonitor()
 
     # Multi-Threaded-Executor für parallele Verarbeitung
